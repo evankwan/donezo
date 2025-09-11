615ab9eade02642d8ed43222c0ba9d2349640049
evankwan <evanyuekwan@gmail.com>
2025-09-11 09:10:04 -0400

    small lint thing

--- src/types/db.ts
+++ src/types/db.ts
@@ -1,11 +1,11 @@
 import type { Todo } from "./todo";
 
 export type TodoList = {
-    id: number;
-    name: string;
-    items: Todo[];
-}
+  id: number;
+  name: string;
+  items: Todo[];
+};
 
 export type LocalStorageDatabase = {
-    lists: TodoList[];
-}
+  lists: TodoList[];
+};
